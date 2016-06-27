c5fd47ac6b3656a21ebcd0a1a91a44556ad9d49b
helten-007 <tommybuksrud@hotmail.com>
2016-06-27 10:26:01 +0200

    Flyttet logikk og fremvisning av headjudge til eget view og egen viewmodell.

--- main/NordicArenaTournament/Areas/Judge/ViewModels/JudgeViewModel.cs
+++ main/NordicArenaTournament/Areas/Judge/ViewModels/JudgeViewModel.cs
@@ -1,11 +1,7 @@
-﻿using System
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NordicArenaDomainModels.Models;
 using NordicArenaTournament.ViewModels;
-
-using NordicArenaTournament.Areas.Speaker.ViewModels;
-using NordicArenaTournament.Database;
 
 namespace NordicArenaTournament.Areas.Judge.ViewModels
 {
@@ -20,23 +16,17 @@ namespace NordicArenaTournament.Areas.Judge.ViewModels
         public bool CanJudge { get; set; }
         public NordicArenaDomainModels.Models.Judge Judge { get; set; }
 
-
-		public List<JudgeHasScoredTuple> JudgeStatus { get; set; }
-
-
         public JudgeViewModel() { }
         /// <summary>
         /// Instantiates a new JudgeViewModel by loading necessary properties from Tournament..
         /// </summary>
         public JudgeViewModel(Tournament tourney, NordicArenaDomainModels.Models.Judge judge)
         {
-			var m = this;
-            m.Tournament = tourney;
-			m.Criteria = tourney.JudgingCriteria.OrderBy(p => p.Id).ToList();
-			m.Judge = judge;
-			m.Contestants = ContestantRunViewModel.CreateListOfCurrentConestants(tourney, judge.Id);
-			m.CanJudge = Contestants.Count > 0 && Contestants[0].Scores.All(p => p.Score == null);
-			m.JudgeStatus = JudgeHasScoredTuple.GetJudgeStatusListForCurrentHeat(tourney);
+			Tournament = tourney;
+			Criteria = tourney.JudgingCriteria.OrderBy(p => p.Id).ToList();
+			Judge = judge;
+			Contestants = ContestantRunViewModel.CreateListOfCurrentConestants(tourney, judge.Id);
+			CanJudge = Contestants.Count > 0 && Contestants[0].Scores.All(p => p.Score == null);
         }
     }
 }
