ab8f7fd44ed40ec76540b4d7e7d7a2c4e5ab83be
helten-007 <tommybuksrud@hotmail.com>
2016-08-22 08:50:39 +0200

    Fjernet kommentert kode, samt unødvendig kode.

--- main/NordicArenaTournament/Scripts/nordic-arena.judge.js
+++ main/NordicArenaTournament/Scripts/nordic-arena.judge.js
@@ -376,20 +376,8 @@ nordicArena.judge.getSliderDataForDidNotSkate = function (contestantIx, criteria
 	var judgingContainer = $("#judging-container-" + contestantIx);
 
 	if (judgingContainer[0].style.display !== 'none') {
-
 		var index = "_" + contestantIx + "_" + criteriaIx;
-
-		//slider.sliderSelector = "#slider" + index;
-
 		slider.amountSelector = "#amount" + index;
-
-		/*
-		Det er mulig at disse linjene kan fjernes fullstendig!!
-		var sliderElem = $(slider.sliderSelector);
-		slider.max = nordicArena.common.parseFloat(sliderElem.attr("max"));
-		slider.min = nordicArena.common.parseFloat(sliderElem.attr("min"));
-		slider.step = nordicArena.common.parseFloat(sliderElem.attr("step"));*/
-
 		$(slider.amountSelector).val(-1);
 	}
 };
@@ -406,13 +394,6 @@ nordicArena.judge.setDidNotSkateScore = function () {
 
 nordicArena.judge.didNotSkate = function () {
 	nordicArena.judge.setDidNotSkateScore();
-	//nordicArena.judge.enableSliders(false);
-	//nordicArena.judge.showProgressBar(true);
-	/*nordicArena.postForm("form", {
-		success: nordicArena.judge.onSubmitScoreSuccess,
-		error: nordicArena.judge.onSubmitScoreError,
-		timeout: 10000
-	});*/
 };
 
 nordicArena.judge.submitScores = function () {
