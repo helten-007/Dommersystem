e4d087c279bb7d1c1cf94e545a0f9cc38d79d0b6
helten-007 <tommybuksrud@hotmail.com>
2016-11-21 14:13:53 +0100

    Fikset bugen med gammel jquery-UI.

--- main/NordicArenaTournament/Scripts/jquery-ui-1.8.24.js
+++ main/NordicArenaTournament/Scripts/jquery-ui-1.8.24.js
@@ -21,6 +21,29 @@
 * Includes: jquery.ui.core.js, jquery.ui.widget.js, jquery.ui.mouse.js, jquery.ui.draggable.js, jquery.ui.droppable.js, jquery.ui.resizable.js, jquery.ui.selectable.js, jquery.ui.sortable.js, jquery.effects.core.js, jquery.effects.blind.js, jquery.effects.bounce.js, jquery.effects.clip.js, jquery.effects.drop.js, jquery.effects.explode.js, jquery.effects.fade.js, jquery.effects.fold.js, jquery.effects.highlight.js, jquery.effects.pulsate.js, jquery.effects.scale.js, jquery.effects.shake.js, jquery.effects.slide.js, jquery.effects.transfer.js, jquery.ui.accordion.js, jquery.ui.autocomplete.js, jquery.ui.button.js, jquery.ui.datepicker.js, jquery.ui.dialog.js, jquery.ui.position.js, jquery.ui.progressbar.js, jquery.ui.slider.js, jquery.ui.tabs.js
 * Copyright (c) 2012 AUTHORS.txt; Licensed MIT */
 
+
+/* 
+ * // START
+ * Only added to avoid conflict when using newer versions of jQuery.
+ * 
+ * */
+
+jQuery.browser = {};
+(function () {
+	jQuery.browser.msie = false;
+	jQuery.browser.version = 0;
+	if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+		jQuery.browser.msie = true;
+		jQuery.browser.version = RegExp.$1;
+	}
+})();
+
+/* 
+ * // END
+ * Only added to avoid conflict when using newer versions of jQuery.
+ * 
+ * */
+
 (function( $, undefined ) {
 
 // prevent duplicate loading
