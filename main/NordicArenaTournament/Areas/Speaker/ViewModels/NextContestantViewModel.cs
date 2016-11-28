f700e31b308a83d957cbb302c9801d21b9f14c3d
helten-007 <tommybuksrud@hotmail.com>
2016-11-28 13:42:25 +0100

    Tester på iPad.

--- main/NordicArenaTournament/Areas/Speaker/ViewModels/NextContestantViewModel.cs
+++ main/NordicArenaTournament/Areas/Speaker/ViewModels/NextContestantViewModel.cs
@@ -6,10 +6,12 @@ namespace NordicArenaTournament.Areas.Speaker.ViewModels
     public class NextContestantViewModel
     {
         public String Name { get; set; }
+		public Contestant Contestant { get; set; }
         public decimal? TotalScore { get; set; }
         public NextContestantViewModel(RoundContestant rc)
         {
             Name = rc.Contestant.Name;
+			Contestant = rc.Contestant;
             TotalScore = rc.TotalScore;
         }
     }
