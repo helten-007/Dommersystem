d7677957b3839058e9eec19b96489bc79311745f
helten-007 <tommybuksrud@hotmail.com>
2017-01-02 10:49:26 +0100

    Små endringer i nye filer. Pusher for å jobbe i lokal branch.

---
+++ main/NordicArenaTournament/Areas/Public/ViewModels/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NordicArenaDomainModels.Models;
+using NordicArenaDomainModels.Resources;
+using NordicArenaTournament.ViewModels;
+
+namespace NordicArenaTournament.Areas.Public.ViewModels
+{
+	public class UserViewModel : _LayoutViewModel
+    {
+		public User User { get; set; }
+		public List<Tournament> Tournaments { get; set; }
+
+		public UserViewModel()
+		{
+
+		}
+    }
+}
